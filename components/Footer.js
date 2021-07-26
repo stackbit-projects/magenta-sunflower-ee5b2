99f71a2e21737ee12ec1d37f25436b9f358e2cad
Stackbit Code Editor <projects@stackbit.com>
2021-07-26 17:00:04 +0000

    Footer.js: changed

--- components/Footer.js
+++ components/Footer.js
@@ -19,7 +19,6 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <
             Tailwind Nextjs Theme
           </Link>
         </div>
