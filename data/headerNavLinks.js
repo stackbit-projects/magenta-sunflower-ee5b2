a17477589c3d0d1808ef8e95d1585e274eeae1bf
Stackbit Code Editor <projects@stackbit.com>
2021-07-26 17:04:59 +0000

    headerNavLinks.js: changed

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/greatworks', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
